26fa405465b26e9761b6b636696b869e7cf2e516
robdll <dilillo.roberto@gmail.com>
2022-09-17 17:18:24 -0400

    adds constants and alias

---
+++ constants/app.constants.js
@@ -0,0 +1,3 @@
+export const MAINPAGE_SEO_TITLE = 'Freecodecamp Issue Tracker'
+export const MAINPAGE_SEO_DESCRIPTION =
+  'Freecodecamp Issue Tracker excercise for the freeCodeCamp Quality Assurance Certification.'
