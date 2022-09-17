b72b9c1c2dc1999c9623aeca509f937e99bd0116
robdll <dilillo.roberto@gmail.com>
2022-09-17 18:02:44 -0400

    add cors for api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,16 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { allowCorsFor } from 'middleware/cors'
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
+  if (req.method == 'OPTIONS') {
+    res.setHeader('Allow', 'OPTIONS')
+    return res.status(202).json({})
+  }
+
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: `Method ${req.method} Not Allowed` })
+  }
+
+  await allowCorsFor(['GET'])(req, res)
+
   res.status(200).json({ name: 'John Doe' })
 }
