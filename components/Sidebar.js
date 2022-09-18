34514c5ec950f73fbdefbe21318f1766e8ae4c5d
robdll <dilillo.roberto@gmail.com>
2022-09-18 17:00:56 -0400

    adds links

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,15 +1,28 @@
 import styles from '@styles/Aside.module.scss'
+import Link from 'next/link'
 
 export default function Sidebar() {
   return (
     <aside className={styles.aside}>
-      <div className={styles.section}>Introduction</div>
-      <div className={styles.section}>ISSUE ROUTES</div>
+      <Link href="#introduction">
+        <div className={styles.section}>Introduction</div>
+      </Link>
+      <Link href="#issues">
+        <div className={styles.section}>ISSUE ROUTES</div>
+      </Link>
       <ul>
-        <li className={styles.route}>GET Issues</li>
-        <li className={styles.route}>PUT Issues</li>
-        <li className={styles.route}>POST Issues</li>
-        <li className={styles.route}>DELETE Issues</li>
+        <Link href="#get">
+          <li className={styles.route}>GET Issues</li>
+        </Link>
+        <Link href="#put">
+          <li className={styles.route}>PUT Issues</li>
+        </Link>
+        <Link href="#post">
+          <li className={styles.route}>POST Issues</li>
+        </Link>
+        <Link href="#delete">
+          <li className={styles.route}>DELETE Issues</li>
+        </Link>
       </ul>
     </aside>
   )
